90d2f746f49add9f49f2b93d018938ab6b24e4b9
MithilaKhan <mithilakhan082@gmail.com>
2024-12-31 20:41:59 +0600

    complete

--- src/app/(website)/consultations/page.tsx
+++ src/app/(website)/consultations/page.tsx
@@ -1,11 +1,12 @@
 import AllConsultations from '@/components/ui/Website/AllConsultations/AllConsultations';
-import React from 'react';
+import React, { Suspense } from 'react';
 
 const ConsultationsPage = () => {
     return (
         <div> 
-            
+             <Suspense fallback={<div>Loading...</div>}> 
          <AllConsultations />   
+             </Suspense>
         </div>
     );
 };
