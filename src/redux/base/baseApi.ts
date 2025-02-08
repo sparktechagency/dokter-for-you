50f5210d39950e5e09966d9793bb7ff3e76ee2a6
Mithila Khan <mithilakhan082@gmail.com>
2025-02-08 12:32:23 +0600

    fdg

--- src/redux/base/baseApi.ts
+++ src/redux/base/baseApi.ts
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://152.42.140.58:5000/api/v1", 
+    baseUrl: "https://api.dokterforyou.com/api/v1", 
       // baseUrl: "https://api.dokterforyou.com/api/v1" ,
     prepareHeaders: (headers) => {
       const token = GetLocalStorage("DokterToken");
@@ -18,5 +18,5 @@ export const baseApi = createApi({
   tagTypes: ["profile"],
 });
 
-// export const imageUrl = "https://api.dokterforyou.com/";
-export const imageUrl = "http://152.42.140.58:5000/"; 
+export const imageUrl = "https://api.dokterforyou.com/"; 
+// export const imageUrl = "http://152.42.140.58:5000/";  
