e7063b83b13f569d3597b23703da6c52e068ef87
Mithila Khan <mithilakhan082@gmail.com>
2025-02-15 16:10:53 +0600

    dfsgs

--- src/components/ui/UserProfile/ProfileHome/ProfileHome.tsx
+++ src/components/ui/UserProfile/ProfileHome/ProfileHome.tsx
@@ -7,10 +7,11 @@ import Consultation from './Tabs/Consultation';
 import DigitalPrescriptionDetails from './Tabs/DigitalPrescriptionDetails';
 import DigitalPrescriptionOrder from './Tabs/DigitalPrescriptionOrder';
 import ChangePassword from './Tabs/ChangePassword';
-import { useConsultationSuccessMutation, useEditProfileMutation, useGetProfileQuery } from '@/redux/features/profile/getProfileSlice';
+import { useEditProfileMutation, useGetProfileQuery } from '@/redux/features/profile/getProfileSlice';
 import Swal from 'sweetalert2';
 import { imageUrl } from '@/redux/base/baseApi';
 import { useSearchParams } from 'next/navigation';
+import { message } from 'antd';
 
 const tabs = [
   { id: "1", label: "Profile Details", component: <ProfileDetails /> },
@@ -27,26 +28,25 @@ const ProfileHome = () => {
   const [imgURL, setImgURL] = useState("");
   const [activeTab, setActiveTab] = useState("1");   
   const searchParams = useSearchParams(); 
-  const [consultationSuccess] = useConsultationSuccessMutation()
-  const sessionId = searchParams.get('session_id');
-  const id = searchParams.get('id');   
+  const isSuccess = searchParams.get('isSuccess'); 
 
-  useEffect(() => {
-    if (id && sessionId) {
-      const data = { id, session_id: sessionId };
-      consultationSuccess(data).then((res) => { 
-        if (res?.data?.success) { 
-           Swal.fire({
-                  
-                   text: "Dear customer, we thank you very much for your trust and payment. Your answers are sent to the doctor. Based on this, the doctor will decide what the best treatment is for you. If this results in a prescription, you will receive a message from the pharmacy that a prescription is ready for you with the next steps. If you have any questions in the meantime, please do not hesitate to ask us (support@dokterforyou.com). Kind regards, team Doctor For You",
-                   icon: "success",
-                   timer: 5000,
-                   showConfirmButton: false
-                 })
-        }
-      })
+
+  useEffect(() => { 
+
+    if(isSuccess){
+      if (isSuccess === "true") {
+        Swal.fire({              
+          text: "Dear customer, we thank you very much for your trust and payment. Your answers are sent to the doctor. Based on this, the doctor will decide what the best treatment is for you. If this results in a prescription, you will receive a message from the pharmacy that a prescription is ready for you with the next steps. If you have any questions in the meantime, please do not hesitate to ask us (support@dokterforyou.com). Kind regards, team Doctor For You",
+          icon: "success",
+          timer: 5000,
+          showConfirmButton: false
+        })
+      }else if (isSuccess === "false") {
+        message.error("Payment Cancelled ! Please try again. ");
+      }  
     }
-  }, [id, sessionId , consultationSuccess]);
+
+}, [isSuccess]); 
 
   useEffect(() => {
     setImgURL(userData?.profile?.startsWith("https")? userData?.profile : `${imageUrl}${userData?.profile}`);
