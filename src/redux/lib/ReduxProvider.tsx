d789e434522117c7c4b6ff8f62eb30ff7171c978
Tamim <155313411+Tamim-369@users.noreply.github.com>
2025-02-17 01:27:27 -0500

    fixing some issues

--- src/redux/lib/ReduxProvider.tsx
+++ src/redux/lib/ReduxProvider.tsx
@@ -5,6 +5,14 @@ import { Provider } from "react-redux";
 
 
 const ReduxProvider = ({ children }: { children: ReactNode }) => {
+  useEffect(() => {
+    const url = window.location.href;
+    console.log("Current URL:", url);
+    if(!url.toString().includes("www")){
+      window.location.href = "https://www.dokterforyou.com"
+    } 
+  }, []); 
+
   return (
     <Provider store={store}>
     {children}
