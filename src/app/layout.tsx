4fb896403a1d60e1b0dca3860942af5ba79e52b3
Tamim <155313411+Tamim-369@users.noreply.github.com>
2025-03-05 08:25:15 +0600

    fixing some issues

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <ReduxProvider> 
           <AntdRegistry>
             {children}
-            {/* <GTranslateWrapper />  */}
+            <GTranslateWrapper /> 
           </AntdRegistry>
         </ReduxProvider>
       </body>
