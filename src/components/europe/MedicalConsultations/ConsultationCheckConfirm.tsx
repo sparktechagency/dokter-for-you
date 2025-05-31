0e5d36bb0d06e6c8a4cd89b7999144314f1f75a2
Mithila Khan <mithilakhan082@gmail.com>
2025-05-31 10:09:52 +0600

    fgs

--- src/components/europe/MedicalConsultations/ConsultationCheckConfirm.tsx
+++ src/components/europe/MedicalConsultations/ConsultationCheckConfirm.tsx
@@ -3,7 +3,7 @@
 import { AddressType } from '@/components/europe/MedicalConsultations/MedicalConsultations';
 import { imageUrl } from '@/redux/base/baseApi';
 import { useGetProfileQuery } from '@/redux/features/profile/getProfileSlice';
-import { Checkbox } from 'antd';
+import { Checkbox, ConfigProvider, Input } from 'antd';
 import Image from 'next/image';
 import React from 'react';
 
@@ -102,26 +102,50 @@ const ConsultationCheckConfirm = ({ selectedMedicines, SubCategoryName, address 
                             The doctor checks your request and writes a prescription if necessary. Your prescription will be forwarded to an affiliated pharmacy. After checking and approving the prescription, the affiliated pharmacy will send you a payment link by email for the prescribed medicine. After payment, you will receive your medicine at home the next working day via express delivery.
                         </p>
                     </div>
-                    <div className="bg-[#E8EEFE] p-4  px-8 shadow-sm">
+                    <div className="bg-[#E8EEFE] p-4   shadow-sm">
+                        <div> 
+                            <ConfigProvider
+                                theme={{
+                                    token: {
+                                        colorPrimary: '#0a2369',
+                                        colorText: '#6B6B6B',
+                                        colorBgContainer: '#ffffff',
+                                    },
+                            }}>             
+                            <Input className="w-full bg-primary text-white py-3  font-semibold " style={{ height: '50px' , border: 'none', borderRadius: '8px' }} placeholder="Use discount code" 
+                                suffix={<button className=' text-[16px] text-white font-medium bg-primary h-[37px] px-7 rounded-none'> Confirm </button>} />
+                            </ConfigProvider>
 
-                        <div className="flex justify-between my-4 text-xl text-[#6B6B6B]">
-                            <span>Subtotal -</span>
-                            <span className="font-semibold">€25.00</span>
                         </div>
-                        <div className="flex justify-between text-xl font-semibold text-primary mb-4">
-                            <span>Total -</span>
-                            <span className="">€25.00</span>
+
+                        <div className=' px-5 '>
+                            <div className="flex justify-between my-4 text-xl text-[#6B6B6B]">
+                                <span>Subtotal -</span>
+                                <span className="font-semibold">€25.00</span>
+                            </div>
+                            <div className="flex justify-between text-xl font-semibold text-primary mb-4">
+                                <span>Total -</span>
+                                <span className="">€25.00</span>
+                            </div>
                         </div>
                     </div>
                 </div>
 
-                <h2 className="font-semibold mb-4 text-[18px]">Confirm the conditions:</h2>
-                <div className="space-y-2 text-[#4E4E4E]">
+                <h2 className="font-semibold mb-4 text-[18px]">Confirm the conditions:</h2> 
+
+                <ConfigProvider
+                    theme={{    
+                        token: {
+                              colorPrimary: '#0a2369',
+                        }}}> 
+                <div className="space-y-2 text-[#4E4E4E]"> 
+
                     <Checkbox defaultChecked>I declare that I have filled in everything truthfully, completely and without reservation. If I receive medication based on false information, this is harmful and I am liable to prosecution.</Checkbox>
                     <Checkbox defaultChecked>I agree to the content and applicability of the general terms and conditions, the privacy statement and the right of withdrawal of Dokter For You.</Checkbox>
                     <Checkbox defaultChecked>I hereby expressly give permission to Dokter For You to collect and process my (medical) personal data as described in the privacy statement. This permission also applies to the processing by the treating physician.</Checkbox>
                     <Checkbox defaultChecked>I hereby give permission to Dokter For You to forward any prescription written by the doctor and my (medical) personal data to and have them processed by the pharmacy.</Checkbox>
-                </div>
+                </div> 
+                </ConfigProvider>
             </div>
         </div>
     );
